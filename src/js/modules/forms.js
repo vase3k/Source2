342404114bfc5629ef838ca67f2d67586e4a6eb4
Vasiliy VV <vase3k@gmail.com>
2024-03-21 10:31:47 +0300

    17 load more db.json

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,6 +1,4 @@
-//import checkNumInputs from './checkNumInputs';
-
-import { setTimeout } from 'core-js';
+import { postData } from '../services/requests';
 
 const forms = () => {
     const form = document.querySelectorAll('form'),
@@ -21,15 +19,6 @@ const forms = () => {
     const path = {
         designer: 'assets/server.php',
         question: 'assets/question.php'
-    };
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: 'POST',
-            body: data
-        });
-
-        return await res.text();
     };
 
     const clearInputs = () => {
